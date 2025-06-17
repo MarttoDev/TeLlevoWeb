9d75ba08f898674ee11450095e972d46e81bd148
marttoclasico <ml.martin.labra@gmail.com>
2025-06-17 05:24:15 -0400

    Primer commit del panel de administración Mascota Feliz

--- js.js
+++ js.js
@@ -20,3 +20,31 @@ document.addEventListener('DOMContentLoaded', () => {
         }, Math.random() * 2000); 
     } 
 }); 
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const domicilioRadio = document.getElementById('domicilioRadio');
+    const direccion = document.getElementById('direccion');
+    const otrosRadio = document.querySelectorAll('input[name=tipo-servicio]');
+
+    otrosRadio.forEach(radio => {
+      radio.addEventListener('change', () => {
+        if (domicilioRadio.checked) {
+          direccion.disabled = false;
+          direccion.required = true;
+        } else {
+          direccion.disabled = true;
+          direccion.required = false;
+        }
+      });
+    });
+  });
+
+  document.getElementById('agendar-form').addEventListener('submit',(e) => {
+    e.preventDefault();
+
+    // Ocultar form
+    document.getElementById('agendar-form').style.display = 'none';
+    //Mostrar mensaje de éxito
+    document.getElementById('reserva-exitosa').style.display = 'block';
+  });
